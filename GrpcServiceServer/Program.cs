858d1749ab3c3dd2c70ecd51a95f703fb055993d
Diego Pulgar <dpp.pul@gmail.com>
2021-12-20 09:12:23 +0100

    Code added for Client reference

--- GrpcServiceServer/Program.cs
+++ GrpcServiceServer/Program.cs
@@ -1,4 +1,6 @@
 using GrpcServiceServer.Services;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,36 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
+PrepareProtoEndpoint(app);
+PrepareProtoBrowseUI(app);
+
 // Configure the HTTP request pipeline.
 app.MapGrpcService<HelloCommunityService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+
+void PrepareProtoEndpoint(WebApplication app)
+{
+
+    var provider = new FileExtensionContentTypeProvider();
+    provider.Mappings.Clear();
+    provider.Mappings[".proto"] = "text/plain";
+    app.UseStaticFiles(new StaticFileOptions
+    {
+
+        FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Protos")),
+        RequestPath = "/proto",
+        ContentTypeProvider = provider
+    });
+}
+
+void PrepareProtoBrowseUI(WebApplication app)
+{
+    app.UseDirectoryBrowser(new DirectoryBrowserOptions
+    {
+        FileProvider = new PhysicalFileProvider(Path.Combine(app.Environment.ContentRootPath, "Protos")),
+        RequestPath = "/proto"
+    });
+}
